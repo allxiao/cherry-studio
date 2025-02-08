1c1a1d195129b2cb4732f931e5c17e09becb40f4
Menghua Xiao <menxiao@microsoft.com>
2025-02-08 17:51:14 +0800

    Add support for Azure OpenAI auth

--- src/preload/index.ts
+++ src/preload/index.ts
@@ -137,7 +137,11 @@ const api = {
   isBinaryExist: (name: string) => ipcRenderer.invoke('app:is-binary-exist', name),
   getBinaryPath: (name: string) => ipcRenderer.invoke('app:get-binary-path', name),
   installUVBinary: () => ipcRenderer.invoke('app:install-uv-binary'),
-  installBunBinary: () => ipcRenderer.invoke('app:install-bun-binary')
+  installBunBinary: () => ipcRenderer.invoke('app:install-bun-binary'),
+
+  azure: {
+    getOpenAiToken: () => ipcRenderer.invoke('azure:getOpenAiToken')
+  }
 }
 
 // Use `contextBridge` APIs to expose Electron APIs to
